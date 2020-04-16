7028e370f06f4575ade4d693f99eba82d41a2c3c
Felix Eduardo Ruiz Ruiz <fruiz@asesoftware.com>
2020-04-16 18:43:25 -0500

    Actualizacion rutina statica proyect 8

--- consclieproject/settings.py
+++ consclieproject/settings.py
@@ -138,6 +138,7 @@ LOGOUT_REDIRECT_URL = '/consdavivienda/accounts/login/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
